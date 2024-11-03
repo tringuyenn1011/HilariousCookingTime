a3c08b7d92a001742b2f7c57b07f5cfb3ca86b72
Tizun71 <gnud0701@gmail.com>
2024-11-03 14:29:31 +0700

    MenuUI

---
+++ Assets/Scripts/MenuUI/Oscillation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillation : MonoBehaviour
+{
+    public float amplitude = 1f; // Biên độ dao động
+    public float frequency = 1f; // Tần số dao động
+    public Vector3 direction = Vector3.right; // Hướng dao động
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float oscillation = Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = startPosition + direction * oscillation;
+    }
+}
